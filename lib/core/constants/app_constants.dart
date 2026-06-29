2f9687df03c3ea55068f8dc89d0f25140899facb
rozakgit <muhammadabdulrozak21@gmail.com>
2026-06-29 13:31:01 +0700

    menghapus /api dari baseUrl karena backend tidak menggunakan path prefix /api

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,9 +1,45 @@
 class AppConstants {
+  static const String appName = 'Bankling';
+  static const String appVersion = '1.0.0';
+
   // Configured to point to the be-bankling backend
-  static const String baseUrl = 'http://192.168.1.5:8081/api';
+  static const String baseUrl = 'http://192.168.1.5:8081';
   
+  static const String apiVersion = '/v1';
   static const int connectTimeout = 30; // seconds
   static const int receiveTimeout = 30; // seconds
 
-  static const String kJwtToken = 'bankling_jwt_token';
+  // Secure Storage Keys
+  static const String kJwtToken = 'jwt_token';
+  static const String kUserData = 'user_data';
+  static const String k2faMethod = 'twofa_method';
+  static const String kFcmToken = 'fcm_token';
+  static const String kAuthVerified = 'auth_verified';
+  static const String kBiometricEnabled = 'biometric_enabled';
+  static const String kAppLockPin = 'app_lock_pin';
+
+  // 2FA Method identifiers
+  static const String twoFaSmtp = 'smtp';
+  static const String twoFaTotp = 'totp';
+  static const String twoFaNotif = 'notif';
+
+  // OTP
+  static const int otpLength = 6;
+  static const int otpResendSeconds = 60;
+
+  // PIN
+  static const int pinLength = 6;
+
+  // Transaction kinds
+  static const String txnTransfer = 'transfer';
+  static const String txnTopup = 'topup';
+  static const String txnPayment = 'payment';
+  static const String txnDeeplink = 'deeplink';
+  static const String txnPulsa = 'pulsa';
+  static const String txnReceived = 'received';
+
+  // OTP types (backend)
+  static const String otpTypeFirebase = 'firebase';
+  static const String otpTypeEmail = 'email';
+  static const String otpTypeTotp = 'totp';
 }
