d5c45689295ce4cd16503623091671978dea1b84
rozakgit <muhammadabdulrozak21@gmail.com>
2026-06-29 13:17:44 +0700

    update desugar_jdk_libs version to 2.1.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,5 +48,5 @@ flutter {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
