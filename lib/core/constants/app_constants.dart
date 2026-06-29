bf45b6e3818bf2cffe14d95d348f1b301ed7ee65
rozakgit <muhammadabdulrozak21@gmail.com>
2026-06-29 12:40:57 +0700

    update port api ke 8081 sesuai backend be-bankling

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
-  // Configured to point to the be-topi_store backend
-  static const String baseUrl = 'http://192.168.1.100:8080/api';
+  // Configured to point to the be-bankling backend
+  static const String baseUrl = 'http://192.168.1.5:8081/api';
   
   static const int connectTimeout = 30; // seconds
   static const int receiveTimeout = 30; // seconds
